d046cd1d0bd158711cdac83c8a2daa9ad3b34d90
blinkelys <martinustomrenslemmen@gmail.com>
2025-07-21 17:15:18 +0200

    Feat: tailwindcss

---
+++ src/components/hero.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="relative flex flex-col items-center justify-center min-h-[400px] w-full">
+        <img src="../assets/hero.jpg" alt="Tresfjord Lyd å Lys Logo" class="absolute top-0 left-0 w-full h-full object-cover">
+        <div class="absolute top-0 left-0 w-full h-full bg-black/50"></div>
+        <div class="relative z-10 flex flex-col items-center justify-center space-y-4">
+            <h1 class="text-6xl font-bold tracking-wide text-white">Tresfjord Lyd å Lys</h1>
+        </div>
+  </div>
+</template>
